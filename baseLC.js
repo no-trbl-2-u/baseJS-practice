d82fe89c615be9808d7cf83fb9b05daa15abd7e4
TJ <no.trbl.2.u@gmail.com>
2018-11-29 14:52:47 -0500

    Began encoding booleans

--- baseLC.js
+++ baseLC.js
@@ -1,25 +1,45 @@
 // KEY
 /*
   a, b => params
-  f => function
+  f, g => functions
   n, m => numbers
+  p, q => booleans
 */
 
 // Lambda Functions
 const I = a => a;                   // Identity
-const M = f => f(f);                // A pply(self)
+const M = f => f(f);                // Apply(self)
 const K = a => b => a;              // Const or first 
 const KI = a => b => b;             // Second
-const C = f => a => b => f(b)(a);   // Flip 
+const C = f => a => b => f(b)(a);   // Flip
+
 
 // Lambda Booleans
-const T = a => b => a;              // True or 
-const F = a => b => b;              // False
+const T = a => b => a;              // True or K or First
+const F = a => b => b;              // False or KI or Second
+
+
+// not :: Boolean -> Boolean
+const not = p => p(F)(T);           // Not or flip (flip(F) === T)
 
 
 // Playground functions
 const add = a => b => a+b;
 const add5 = add(5);
+const first = T;
+const second = F;
+const _first = C(F);        // Extensionally equal to T
+const _second = C(T);       // Extensionally equal to F
+
+//                          Intentional vs Extensional equality
+// Extensional equality is if 2 fn given the same in, provide the same out
+// Intentional equality is a comparison of the "guts" of a fn
+// Example of (K/T && KI/F) vs (flip && not)
+// console.log(first(1)(2) === 1);     // true
+// console.log(second(1)(2) === 2);    // true
+// console.log(_first(1)(2) === 1);    // true 
+// console.log(_second(1)(2) === 2);   // true
+
 
 
 // Playground Data
