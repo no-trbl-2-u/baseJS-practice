558ae06514aacb44fa14d3bd8e2b91d1b971fa8c
TJ <no.trbl.2.u@gmail.com>
2018-11-29 15:12:51 -0500

    Added Or and And

--- baseLC.js
+++ baseLC.js
@@ -13,37 +13,34 @@ const K = a => b => a;              // Const or first
 const KI = a => b => b;             // Second
 const C = f => a => b => f(b)(a);   // Flip
 
-
 // Lambda Booleans
 const T = a => b => a;              // True or K or First
 const F = a => b => b;              // False or KI or Second
+const not = p => p(F)(T);           // Not or flip !combinator
+const and = p => q => p(q)(p);      // And
+const or = p => q => p(p)(q)        // Or {M*} ** discovered :D **
+const beq = p => q => p(q)(not(q))   // Boolean Equality
+const _beq = p => q => p(q(T)(F))(q(F)(T))
 
-
-// not :: Boolean -> Boolean
-const not = p => p(F)(T);           // Not or flip (flip(F) === T)
 
 
 // Playground functions
 const add = a => b => a+b;
 const add5 = add(5);
-const first = T;
-const second = F;
-const _first = C(F);        // Extensionally equal to T
-const _second = C(T);       // Extensionally equal to F
+
 
 //                          Intentional vs Extensional equality
 // Extensional equality is if 2 fn given the same in, provide the same out
 // Intentional equality is a comparison of the "guts" of a fn
+
+const _first = T;
+const _second = F;
+const __first = C(F);        // Extensionally equal to T
+const __second = C(T);       // Extensionally equal to F
+
 // Example of (K/T && KI/F) vs (flip && not)
-// console.log(first(1)(2) === 1);     // true
-// console.log(second(1)(2) === 2);    // true
-// console.log(_first(1)(2) === 1);    // true 
-// console.log(_second(1)(2) === 2);   // true
 
-
-
-// Playground Data
-const str = 'BaseLC';
-const arr = [1, 2, 'a'];
-const charA = 'a';
-const charB = 'b';
+// console.log(_first(1)(2) === 1);     // true
+// console.log(_second(1)(2) === 2);    // true
+// console.log(__first(1)(2) === 1);    // true 
+// console.log(__second(1)(2) === 2);   // true
